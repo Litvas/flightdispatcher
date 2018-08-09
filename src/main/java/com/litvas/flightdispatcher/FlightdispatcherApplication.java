7e089673a385ae06e6a613414b897b1384743f5e
as <litvas13@gmail.com>
2018-08-09 21:11:06 +0300

    generate progect from start.spring.io

---
+++ src/main/java/com/litvas/flightdispatcher/FlightdispatcherApplication.java
@@ -0,0 +1,12 @@
+package com.litvas.flightdispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightdispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightdispatcherApplication.class, args);
+	}
+}
